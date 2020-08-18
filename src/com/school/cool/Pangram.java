ce212e12a3424d6957d31e958e431f57094d7b79
carlosm <carlosjmarinj@gmail.com>
2020-08-17 20:48:11 -0500

    pangram checker, but checking boolean first

---
+++ src/com/school/cool/Pangram.java
@@ -0,0 +1,13 @@
+package com.school.cool;
+
+public class Pangram {
+    public static void main(String args[])
+    {
+        //TYPE DECLARATION DEFAULT VALUE OF boolean IS false
+        boolean b;
+        System.out.println("Java boolean Data Type Example");
+        //ASSIGN A VALUE
+        b = false;
+        System.out.println("Assigned boolean Value : " + b);
+    }
+}
